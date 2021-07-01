f8342c853f3aa98cf2ce47529f087a79f2792196
heyset <c.inaciomatheus@gmail.com>
2021-07-01 11:58:42 -0300

    corrige roles

--- demos/jest_mock/end/__tests__/weather.test.js
+++ demos/jest_mock/end/__tests__/weather.test.js
@@ -5,12 +5,18 @@ const { queryForecast } = require('../src/weather');
 const { fetchPlaces, fetchWeather } = require('../src/services/weatherApi');
 
 describe('A função queryForecast', () => {
-  it('retorna o objeto correto', async () => {
-    const expectedForecast = require('./fixtures/weather.json');
-
-    expect(await queryForecast({ query: 'brasília' })).toEqual(expectedForecast);
+  it('chama as funções de fetch com os parâmetros corretos', async () => {
+    await queryForecast({ query: 'brasília' });
 
     expect(fetchPlaces).toHaveBeenCalledWith('brasília');
     expect(fetchWeather).toHaveBeenCalledWith(455819);
+  });
+
+  it('retorna o objeto correto', async () => {
+    const expectedForecast = require('./fixtures/weather.json');
+
+    const forecast = await queryForecast({ query: 'brasília' });
+
+    expect(forecast).toEqual(expectedForecast);
   });
 });
--- demos/jest_mock/start/__tests__/weather.test.js
+++ demos/jest_mock/start/__tests__/weather.test.js
@@ -4,6 +4,9 @@ const { queryForecast } = require('../src/weather');
 const { fetchPlaces, fetchWeather } = require('../src/services/weatherApi');
 
 describe('A função queryForecast', () => {
+  it('chama as funções de fetch com os parâmetros corretos', async () => {
+  });
+
   it('retorna o objeto correto', async () => {
   });
 });
